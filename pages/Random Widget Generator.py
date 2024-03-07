b1cbcd59c1950f087da4dc9183865217aa881900
zendeaashay <148306495+zendeaashay@users.noreply.github.com>
2024-03-07 00:14:22 -0500

    Update Random Widget Generator.py

--- pages/Random Widget Generator.py
+++ pages/Random Widget Generator.py
@@ -6,7 +6,7 @@ Created on Thu Mar  7 00:09:55 2024
 @author: aashayzende
 """
 from streamlit_faker import get_streamlit_faker
-faker = get_streamlit_faker(seed=seed)
+st_faker = get_streamlit_faker(seed=seed)
 st_faker.subheader()
 st_faker.markdown()
 st_faker.selectbox()
