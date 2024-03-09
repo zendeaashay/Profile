56833a2c9eaf3d9d206462b4caa7092b99b87971
zendeaashay <148306495+zendeaashay@users.noreply.github.com>
2024-03-08 20:10:04 -0500

    Update AI Chat.py

--- pages/AI Chat.py
+++ pages/AI Chat.py
@@ -1,7 +1,6 @@
-from llama_index import VectorStoreIndex, ServiceContext, Document
+
 from llama_index.llms import OpenAI
 import openai
-from llama_index import SimpleDirectoryReader
 import streamlit as st
 from llama_index.core import VectorStoreIndex,SimpleDirectoryReader,ServiceContext
 
