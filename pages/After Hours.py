6dee23d30b7b84ca81b47ae934adaf74c3ad6ba6
zendeaashay <148306495+zendeaashay@users.noreply.github.com>
2024-03-10 16:23:22 -0400

    Update After Hours.py

--- pages/After Hours.py
+++ pages/After Hours.py
@@ -14,7 +14,7 @@ def check_password():
             st.session_state["password_correct"] = False
 
     # Return True if the password is validated.
-    if st.session_state.get("password_correct", True):
+    if st.session_state.get("password_correct", False):
         return True
 
     # Show input for password.
@@ -25,9 +25,9 @@ def check_password():
         st.error("😕 Password incorrect")
     return False
 
+
 if not check_password():
     st.stop()  # Do not continue if check_password is not True.
-
 
 # Main Streamlit app starts here
 st.write("Here goes your normal Streamlit app...")
