7f13cc03e8714720ed033fbb7261f55d59cec7fb
zendeaashay <148306495+zendeaashay@users.noreply.github.com>
2024-03-14 21:39:37 -0400

    Update database.py

---
+++ pages/database.py
@@ -0,0 +1,22 @@
+from google.cloud.sql.connector import Connector
+import sqlalchemy
+
+# initialize Connector object
+connector = Connector()
+
+# function to return the database connection
+def getconn() -> pymysql.connections.Connection:
+    conn: pymysql.connections.Connection = connector.connect(
+        "exalted-ability-417323:us-east4:ash247",
+        "pymysql",
+        user="my-user",
+        password="my-password",
+        db="my-db-name"
+    )
+    return conn
+
+# create connection pool
+pool = sqlalchemy.create_engine(
+    "mysql+pymysql://",
+    creator=getconn,
+)
