31c4c1f3b66cdb11f69936bf656724ddac9c3817
zendeaashay <148306495+zendeaashay@users.noreply.github.com>
2024-03-16 13:46:11 -0400

    Update Consulting Insights.py

--- pages/Consulting Insights.py
+++ pages/Consulting Insights.py
@@ -1,18 +1,11 @@
 import streamlit as st
-import PyPDF2
-import base64
+from streamlit_pdf_viewer import st_pdf_viewer
 
-# Function to read PDF file and convert to base64
-def get_base64_of_pdf_data(pdf_file_path):
-    with open(pdf_file_path, "rb") as f:
-        pdf_data = f.read()
-    return base64.b64encode(pdf_data).decode('utf-8')
+# Assuming your Streamlit app has multiple pages, use an if statement or a function to control page display
+def show_pdf_viewer_page():
+    # Load the PDF file into the app
+    with open("eComm India.pdf", "rb") as pdf_file:
+        st_pdf_viewer(pdf_file)
 
-# Embeds the PDF file in the Streamlit app
-def st_display_pdf(pdf_file_path):
-    pdf_base64 = get_base64_of_pdf_data(pdf_file_path)
-    pdf_display = f'<iframe src="data:application/pdf;base64,{pdf_base64}" width="700" height="1000" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
-
-# Display the PDF
-st_display_pdf("eComm India.pdf")
+# Call the function to display the PDF viewer page
+show_pdf_viewer_page()
