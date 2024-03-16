61aa1d452a13f9be1d1ff44b7007e40ef3ab7d97
zendeaashay <148306495+zendeaashay@users.noreply.github.com>
2024-03-16 13:59:48 -0400

    Update Consulting Insights.py

--- pages/Consulting Insights.py
+++ pages/Consulting Insights.py
@@ -1,25 +1,11 @@
 import streamlit as st
 from streamlit_pdf_viewer import pdf_viewer
 
-def show_resume():
-    st.title('My Resume')
+def show_pdf():
+    st.title('My Consulting Analysis and Insigths on the Indian E-Tailing Market')
 
-    # You can add more content here as needed
-    st.write("Here is my resume showcasing my experience and skills.")
+    pdf_viewer("eComm India.pdf", width=850, height=1000)
 
-    # Display the resume PDF
-    with open("eComm India.pdf", "rb") as file:
-        pdf_viewer(file, width=850, height=1000)
-
-    # Link to download resume
-    resume_link = 'eComm India.pdf'  # Make sure this is the correct path to your resume PDF
-    with open(resume_link, "rb") as file:
-        st.download_button(
-            label="Download Resume",
-            data=file,
-            file_name="Aashay Zende - Resume.pdf",
-            mime="application/pdf"
-        )
 
 # Execute the function to display the resume
-show_resume()
+show_pdf()
