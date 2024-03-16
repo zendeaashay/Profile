1bad9af6d90fde43f317c3818a4fd5c26226f005
zendeaashay <148306495+zendeaashay@users.noreply.github.com>
2024-03-16 14:00:54 -0400

    Update Consulting Insights.py

--- pages/Consulting Insights.py
+++ pages/Consulting Insights.py
@@ -4,7 +4,7 @@ from streamlit_pdf_viewer import pdf_viewer
 def show_pdf():
     st.title('My Consulting Analysis and Insigths on the Indian E-Tailing Market')
 
-    pdf_viewer("eComm India.pdf", width=12000, height=1000)
+    pdf_viewer("eComm India.pdf", width=500, height=800)
 
 
 # Execute the function to display the resume
