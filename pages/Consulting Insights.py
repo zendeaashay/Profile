936d01f05eae94777b06d7919feb729e549acd15
zendeaashay <148306495+zendeaashay@users.noreply.github.com>
2024-03-16 14:07:52 -0400

    Update Consulting Insights.py

--- pages/Consulting Insights.py
+++ pages/Consulting Insights.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_pdf_viewer import pdf_viewer
 
 def show_pdf():
-    st.title('My Consulting Analysis and Insigths on the Indian E-Tailing Market')
+    st.title('Consulting Analysis and Insigths on the Indian E-Tailing Market')
 
     pdf_viewer("eComm India.pdf", width=700, height=487)
 
