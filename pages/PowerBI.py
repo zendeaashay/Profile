88e2fee7bb3f193781083b45bf44c9d863d69e59
zendeaashay <148306495+zendeaashay@users.noreply.github.com>
2024-04-22 16:14:54 -0400

    Update PowerBI.py

--- pages/PowerBI.py
+++ pages/PowerBI.py
@@ -11,7 +11,7 @@ import pandas as pd
 
 
 
-st.set_page_config(page_title="A PowerBI dasboard created by my group on the Happy Cow Company sales", page_icon="🌟", layout="wide")
+st.set_page_config(page_title="A PowerBI dashboard created by my group on the Happy Cow Company sales", page_icon="🌟", layout="wide")
 hide_default_format = """
        <style>
        #MainMenu {visibility: hidden; }
@@ -19,7 +19,9 @@ hide_default_format = """
        </style>
        """
 st.markdown(hide_default_format, unsafe_allow_html=True)
+
+# Replace the old iframe code with the new one
 st.markdown("""
-    <iframe width="600" height="606" src="https://app.powerbi.com/reportEmbed?reportId=e8de6e97-8e70-42ac-a8b1-14a041c9be78&autoAuth=true&ctid=a8eec281-aaa3-4dae-ac9b-9a398b9215e7" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <iframe title="Happy Cow dashboard" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=e8de6e97-8e70-42ac-a8b1-14a041c9be78&autoAuth=true&ctid=a8eec281-aaa3-4dae-ac9b-9a398b9215e7" frameborder="0" allowFullScreen="true"></iframe>
     """, unsafe_allow_html=True)
     
